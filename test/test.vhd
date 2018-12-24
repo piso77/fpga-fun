486285164496c8b8b31b48e8c5e1d5eb006558c2
Paolo Pisati <p.pisati@gmail.com>
2018-12-24 15:43:07 +0100

    test: extend testing to ssegs display
    Signed-off-by: Paolo Pisati <p.pisati@gmail.com>

--- test/test.vhd
+++ test/test.vhd
@@ -22,7 +22,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -32,17 +32,22 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity test is
 	generic(WIDTH: integer := 8);
 	port(
+		clk: in std_logic;
 		switch : in  STD_LOGIC_VECTOR (WIDTH-1 downto 0);
 		joy : in STD_LOGIC_VECTOR(4 downto 0);
-		led : out  STD_LOGIC_VECTOR (WIDTH-1 downto 0)
+		led : out  STD_LOGIC_VECTOR (WIDTH-1 downto 0);
+		an: out STD_LOGIC_VECTOR (3 downto 0);
+		sseg: out STD_LOGIC_VECTOR (7 downto 0)
 	);
 end test;
 
 architecture arch of test is
-	constant JOYS: integer := 5; -- joy btns 
+	constant JOYS: integer := 5; -- joy btns
+	signal a,b: unsigned(7 downto 0);
+	signal sum: std_logic_vector(7 downto 0);
 begin
 
-assert (WIDTH >= JOYS) report "WIDTH too small: minimum " & integer'image(JOYS) severity failure;
+assert (WIDTH >= JOYS+3) report "WIDTH too small: minimum " & integer'image(JOYS) severity failure;
 
 for_loop_joy:
 for i in 0 to JOYS-1 generate
@@ -57,5 +62,21 @@ for i in JOYS to (WIDTH-1) generate
 	led(i) <= switch(i);
 end generate;
 
+ff: entity work.disp_hex_mux(arch)
+port map(
+	clk => clk,
+	reset => '0',
+	hex0 => switch(3 downto 0),
+	hex1 => switch(7 downto 4),
+	hex2 => sum(7 downto 4),
+	hex3 => sum(3 downto 0),
+	dp_in => "1101",
+	an => an,
+	sseg => sseg
+);
+
+a <= unsigned("0000" & switch(7 downto 4));
+b <= unsigned("0000" & switch(3 downto 0));
+sum <= std_logic_vector(a + b);
 
 end arch;
