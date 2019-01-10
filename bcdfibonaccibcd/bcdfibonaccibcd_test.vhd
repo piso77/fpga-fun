b32ddcbd01494eef41a2e43d452578bd49944bf5
Paolo Pisati <p.pisati@gmail.com>
2019-01-10 17:18:02 +0100

    bcdfibonaccibcd: plub all pieces together
    Signed-off-by: Paolo Pisati <p.pisati@gmail.com>

--- bcdfibonaccibcd/bcdfibonaccibcd_test.vhd
+++ bcdfibonaccibcd/bcdfibonaccibcd_test.vhd
@@ -13,42 +13,65 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity bcdfibonaccibcd_test is
 	port(
 		clk : in  STD_LOGIC;
-		switch : in  STD_LOGIC_VECTOR (6 downto 0);
-		led : out  STD_LOGIC_VECTOR (1 downto 0);
+		switch : in  STD_LOGIC_VECTOR (7 downto 0);
+		btn : STD_LOGIC;
+		--led : out  STD_LOGIC_VECTOR (7 downto 0);
 		an : out  STD_LOGIC_VECTOR (3 downto 0);
 		sseg : out  STD_LOGIC_VECTOR (7 downto 0)
 	);
 end bcdfibonaccibcd_test;
 
 architecture arch of bcdfibonaccibcd_test is
-	signal tmp : std_logic_vector(19 downto 0);
-	signal reset, start : std_logic;
+	signal start, donebcd, donefib, donebin : std_logic;
+	signal binary : std_logic_vector(6 downto 0);
+	signal fibo : std_logic_vector(19 downto 0);
+	signal bcds : std_logic_vector(15 downto 0);
 begin
 
-reset <= switch(0);
-start <= switch(1);
+start <= not btn;
+
+bcdtobin: entity work.bcdtobin(arch)
+	port map(
+		clk => clk,
+		start => start,
+		done => donebcd,
+		bcd => switch,
+		bin => binary
+	);
 
 fib: entity work.fibonacci(arch)
 	port map(
 		clk => clk,
-		reset => reset,
-		start => start,
-		i => switch(6 downto 2),
-		ready => led(0),
-		done_tick => led(1),
-		f => tmp
+		reset => '0',
+		start => donebcd,
+		i => binary(4 downto 0),
+		ready => open,
+		done_tick => donefib,
+		f => fibo
 	);
 
---led(7 downto 2) <= tmp(5 downto 0);
+bintobcd: entity work.bin2bcd(arch)
+	port map(
+		clk => clk,
+		reset => '0',
+		start => donefib,
+		bin => fibo(12 downto 0),
+		ready => open,
+		done_tick => donebin,
+		bcd0 => bcds(15 downto 12),
+		bcd1 => bcds(11 downto 8),
+		bcd2 => bcds(7 downto 4),
+		bcd3 => bcds(3 downto 0)
+	);
 
 dispmux: entity work.disp_hex_mux(arch)
 	port map(
 		clk => clk,
-		reset => reset,
-		hex0 => tmp(15 downto 12),
-		hex1 => tmp(11 downto 8),
-		hex2 => tmp(7 downto 4),
-		hex3 => tmp(3 downto 0),
+		reset => '0',
+		hex3 => bcds(15 downto 12),
+		hex2 => bcds(11 downto 8),
+		hex1 => bcds(7 downto 4),
+		hex0 => bcds(3 downto 0),
 		dp_in => "1111",
 		an => an,
 		sseg => sseg
@@ -56,3 +79,40 @@ dispmux: entity work.disp_hex_mux(arch)
 
 end arch;
 
+
+
+--architecture test of bcdfibonaccibcd_test is
+--	signal tmp : std_logic_vector(19 downto 0);
+--	signal reset, start : std_logic;
+--begin
+--
+--reset <= switch(0);
+--start <= switch(1);
+--
+--fib: entity work.fibonacci(arch)
+--	port map(
+--		clk => clk,
+--		reset => reset,
+--		start => start,
+--		i => switch(6 downto 2),
+--		ready => led(0),
+--		done_tick => led(1),
+--		f => tmp
+--	);
+--
+--led(7 downto 2) <= tmp(5 downto 0);
+--
+--dispmux: entity work.disp_hex_mux(arch)
+--	port map(
+--		clk => clk,
+--		reset => reset,
+--		hex0 => tmp(15 downto 12),
+--		hex1 => tmp(11 downto 8),
+--		hex2 => tmp(7 downto 4),
+--		hex3 => tmp(3 downto 0),
+--		dp_in => "1111",
+--		an => an,
+--		sseg => sseg
+--	);
+--
+--end test;
