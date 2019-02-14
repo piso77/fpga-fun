1a6220300b01e54c72af5f10d88f54c1bb2332b4
Paolo Pisati <p.pisati@gmail.com>
2019-02-14 19:30:14 +0100

    vga_test: fist test using an image embedded in bram (as a single port rom)
    Signed-off-by: Paolo Pisati <p.pisati@gmail.com>

--- vga_test/vga_test.vhd
+++ vga_test/vga_test.vhd
@@ -17,6 +17,9 @@ architecture arch of vga_test is
 	signal clko 	: std_logic;
 	signal hcount	: unsigned(9 downto 0) := (others => '0');
 	signal vcount	: unsigned(9 downto 0) := (others => '0');
+	signal addr : std_logic_vector(15 downto 0) := (others => '0');
+	signal tmp : std_logic_vector(19 downto 0) := (others => '0');
+	signal data : std_logic_vector(7 downto 0) := (others => '0');
 begin
 
 clk25mhz : entity work.clk_wiz_v3_6(xilinx)
@@ -24,6 +27,13 @@ clk25mhz : entity work.clk_wiz_v3_6(xilinx)
     CLK_IN1 => clk,
     CLK_OUT1 => clko
 	);
+
+vrom : entity work.blk_mem_gen_v7_3(blk_mem_gen_v7_3_a)
+  PORT MAP (
+    clka => clko,
+    addra => addr,
+    douta => data
+  );
 
 process(clko)
 begin
@@ -41,7 +51,10 @@ begin
 	end if;
 end process;
 
-process(hcount, vcount)
+tmp <= std_logic_vector(shift_right(hcount, 1) + (shift_right(vcount, 1) * 320));
+addr <= tmp(15 downto 0);
+
+process(hcount, vcount, data)
 begin
 	vga_hsync <= '1';
 	vga_vsync <= '1';
@@ -57,20 +70,25 @@ begin
 		vga_hsync <= '0';
 	end if;
 
-	if hcount < 640 then
-		if vcount < 160 then
-			vga_blue 	<= "00";
-			vga_green 	<= "000";
-			vga_red 		<= "111";
-		elsif vcount < 320 then
-			vga_blue 	<= "00";
-			vga_green 	<= "111";
-			vga_red 		<= "000";
-		elsif vcount < 480 then
-			vga_blue 	<= "11";
-			vga_green 	<= "000";
-			vga_red 		<= "000";
-		end if;
+--	if hcount < 640 then
+--		if vcount < 160 then
+--			vga_blue 	<= "00";
+--			vga_green 	<= "000";
+--			vga_red 		<= "111";
+--		elsif vcount < 320 then
+--			vga_blue 	<= "00";
+--			vga_green 	<= "111";
+--			vga_red 		<= "000";
+--		elsif vcount < 480 then
+--			vga_blue 	<= "11";
+--			vga_green 	<= "000";
+--			vga_red 		<= "000";
+--		end if;
+--	end if;
+	if hcount < 640 and vcount < 400 then
+		vga_red <= data(7 downto 5);
+		vga_green <= data(4 downto 2);
+		vga_blue <= data(1 downto 0);
 	end if;
 end process;
 
