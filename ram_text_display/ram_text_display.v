841c16662fc154a7a271c7e950c3055a2ebbac94
Paolo Pisati <p.pisati@gmail.com>
2019-06-16 15:03:27 +0200

    ram_text_display: initial import
    Signed-off-by: Paolo Pisati <p.pisati@gmail.com>

---
+++ ram_text_display/ram_text_display.v
@@ -0,0 +1,12 @@
+/*
+Displays a grid of digits on the CRT using a RAM module.
+*/
+
+module ram_text_top(clk, reset, hsync, vsync, rgb);
+input clk, reset;
+output hsync, vsync;
+output [2:0] rgb;
+
+
+
+endmodule
