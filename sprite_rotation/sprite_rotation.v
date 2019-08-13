f2684735f3cc2dd649466ae892c5df91a410cc8a
Paolo Pisati <p.pisati@gmail.com>
2019-08-13 10:38:33 +0200

    sprite_rotation: move sprite out to data file
    Signed-off-by: Paolo Pisati <p.pisati@gmail.com>

--- sprite_rotation/sprite_rotation.v
+++ sprite_rotation/sprite_rotation.v
@@ -15,93 +15,10 @@ module tank_bitmap(addr, bits);
   reg [15:0] bitarray[0:255];
   
   assign bits = (addr[0]) ? bitarray[addr>>1][15:8] : bitarray[addr>>1][7:0];
+
+  initial
+	$readmemb("tank.hex", bitarray);
   
-  initial begin/*{w:16,h:16,bpw:16,count:5}*/
-    bitarray['h00] = 16'b11110000000;
-    bitarray['h01] = 16'b11110000000;
-    bitarray['h02] = 16'b1100000000;
-    bitarray['h03] = 16'b1100000000;
-    bitarray['h04] = 16'b111101101111000;
-    bitarray['h05] = 16'b111101101111000;
-    bitarray['h06] = 16'b111111111111000;
-    bitarray['h07] = 16'b111111111111000;
-    bitarray['h08] = 16'b111111111111000;
-    bitarray['h09] = 16'b111111111111000;
-    bitarray['h0a] = 16'b111111111111000;
-    bitarray['h0b] = 16'b111100001111000;
-    bitarray['h0c] = 16'b111100001111000;
-    bitarray['h0d] = 16'b0;
-    bitarray['h0e] = 16'b0;
-    bitarray['h0f] = 16'b0;
-    
-    bitarray['h10] = 16'b111000000000;
-    bitarray['h11] = 16'b1111000000000;
-    bitarray['h12] = 16'b1111000000000;
-    bitarray['h13] = 16'b11000000000;
-    bitarray['h14] = 16'b11101110000;
-    bitarray['h15] = 16'b1101110000;
-    bitarray['h16] = 16'b111101111110000;
-    bitarray['h17] = 16'b111101111111000;
-    bitarray['h18] = 16'b111111111111000;
-    bitarray['h19] = 16'b11111111111000;
-    bitarray['h1a] = 16'b11111111111100;
-    bitarray['h1b] = 16'b11111111111100;
-    bitarray['h1c] = 16'b11111001111100;
-    bitarray['h1d] = 16'b1111001110000;
-    bitarray['h1e] = 16'b1111000000000;
-    bitarray['h1f] = 16'b1100000000000;
-    
-    bitarray['h20] = 16'b0;
-    bitarray['h21] = 16'b0;
-    bitarray['h22] = 16'b11000011000000;
-    bitarray['h23] = 16'b111000111100000;
-    bitarray['h24] = 16'b111101111110000;
-    bitarray['h25] = 16'b1110111111000;
-    bitarray['h26] = 16'b111111111100;
-    bitarray['h27] = 16'b11111111110;
-    bitarray['h28] = 16'b11011111111110;
-    bitarray['h29] = 16'b111111111111100;
-    bitarray['h2a] = 16'b111111111001000;
-    bitarray['h2b] = 16'b11111110000000;
-    bitarray['h2c] = 16'b1111100000000;
-    bitarray['h2d] = 16'b111110000000;
-    bitarray['h2e] = 16'b11110000000;
-    bitarray['h2f] = 16'b1100000000;
-
-    bitarray['h30] = 16'b0;
-    bitarray['h31] = 16'b0;
-    bitarray['h32] = 16'b110000000;
-    bitarray['h33] = 16'b100001111000000;
-    bitarray['h34] = 16'b1110001111110000;
-    bitarray['h35] = 16'b1111010111111100;
-    bitarray['h36] = 16'b1111111111111111;
-    bitarray['h37] = 16'b1111111111111;
-    bitarray['h38] = 16'b11111111110;
-    bitarray['h39] = 16'b101111111110;
-    bitarray['h3a] = 16'b1111111101100;
-    bitarray['h3b] = 16'b11111111000000;
-    bitarray['h3c] = 16'b1111111100000;
-    bitarray['h3d] = 16'b11111110000;
-    bitarray['h3e] = 16'b111100000;
-    bitarray['h3f] = 16'b1100000;
-
-    bitarray['h40] = 16'b0;
-    bitarray['h41] = 16'b0;
-    bitarray['h42] = 16'b0;
-    bitarray['h43] = 16'b111111111000;
-    bitarray['h44] = 16'b111111111000;
-    bitarray['h45] = 16'b111111111000;
-    bitarray['h46] = 16'b111111111000;
-    bitarray['h47] = 16'b1100001111100000;
-    bitarray['h48] = 16'b1111111111100000;
-    bitarray['h49] = 16'b1111111111100000;
-    bitarray['h4a] = 16'b1100001111100000;
-    bitarray['h4b] = 16'b111111111000;
-    bitarray['h4c] = 16'b111111111000;
-    bitarray['h4d] = 16'b111111111000;
-    bitarray['h4e] = 16'b111111111000;
-    bitarray['h4f] = 16'b0;
-  end
 endmodule
 
 // 16x16 sprinte renderer that supports rotation
