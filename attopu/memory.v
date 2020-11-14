cb012c6fc61d34b07534c36d6298e737bc04a78c
Paolo Pisati <paolo.pisati@canonical.com>
2020-11-14 12:58:44 +0100

    Revert "attopu: increase memory module to 64K words to fix all verilator warning"
    This reverts commit c90ff526fa74ff1be5fd8a4fbac20f741f58c564.
    
    A 64K x 16b word mem module was impossible to synthetize on ico or papilio using
    BRAM, revert back to a 1024 x 16 size.
    
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- attopu/memory.v
+++ attopu/memory.v
@@ -6,7 +6,7 @@ module memory(input clk,
               input [15:0] dDataIn,
               output [15:0] dDataOut);
 
-       reg [15:0] memArray [65535:0]; // Notice that Im not filling in all of memory with the memory array, ie, addresses can only from $0000 to $03ff
+       reg [15:0] memArray [1023:0]; // Notice that Im not filling in all of memory with the memory array, ie, addresses can only from $0000 to $03ff
 
         initial begin
           // Load in the program/initial memory state into the memory module
@@ -15,11 +15,11 @@ module memory(input clk,
 
         always @(posedge clk) begin
           if (dWE) begin // When the WE line is asserted, write into memory at the given address
-            memArray[dAddr[15:0]] <= dDataIn; // Limit the range of the addresses
+            memArray[dAddr[9:0]] <= dDataIn; // Limit the range of the addresses
           end
         end
 
-        assign dDataOut = memArray[dAddr[15:0]];
-        assign iDataOut = memArray[iAddr[15:0]];
+        assign dDataOut = memArray[dAddr[9:0]];
+        assign iDataOut = memArray[iAddr[9:0]];
 
 endmodule
