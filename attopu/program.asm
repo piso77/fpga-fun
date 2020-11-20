394d49cc56bbbc82dd73c2b3326f81c9d5da4eab
Paolo Pisati <paolo.pisati@canonical.com>
2020-11-20 18:15:29 +0100

    attopu: program: more instructions coverage
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- attopu/program.asm
+++ attopu/program.asm
@@ -3,15 +3,16 @@
 .len	1024
 .width	16
 
-LD	r0, #$09
-LD	r1, #$0a
-LD	r3, #$0e
+LD	r0, #$0a
+LD	r1, #$0b
 ADD	r2, r0, r1
-ST	r2, r3
-LD	r3, #$06
+ST	r2, #$0a		; st rs, $addr	-- MEM[$addr] = rs -- 1010|0000|0001|0100 => A014
+LD	r3, #$0e
+ST	r2, r3			; st rs, ra		-- MEM[ra] = rs
+RESET
 RESET
 RESET
 RESET
 
-; every data item is .width padded
-.data 0001 0002 0003 0004 0005 0006
+; every data item is .width padded and decimal
+.data 0010 0002 0003 0004 0015 0006
