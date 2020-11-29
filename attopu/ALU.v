538e7dafec9c2763bf8c61b1074d64426bac02dc
Paolo Pisati <paolo.pisati@canonical.com>
2020-11-29 16:34:47 +0100

    attopu: ALU: implement the carry flag
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- attopu/ALU.v
+++ attopu/ALU.v
@@ -4,37 +4,46 @@ module ALU(
 	input [6:0] op,
 	input [15:0] in1,
 	input [15:0] in2,
-	output reg [15:0] out,
+	output [15:0] out,
+	output reg cFlag,
 	output reg zFlag
 );
 
-	reg zFlagNext;
+	reg cFlagNext, zFlagNext;
+	reg [16:0] tmpout;
 
 	// Z flag register
 	always @(posedge clk, posedge rst) begin
 		if (rst) begin
+			cFlag <= 1'b0;
 			zFlag <= 1'b0;
 		end else begin
+			cFlag <= cFlagNext;
 			zFlag <= zFlagNext;
 		end
 	end
+
+	assign out = tmpout[15:0];
 
 	// ALU Logic
 	always @(*) begin
 		// Defaults -- I do this to: 1) make sure there are no latches, 2) list all
 		// variables set by this block
-		out = 16'd0;
-		zFlagNext = zFlag; // Note, according to our ISA, the z flag only changes when an ADD is performed, otherwise it should retain its value
+		tmpout = 17'd0;
+	 // Note, according to our ISA, the z flag only changes when an ADD is performed, otherwise it should retain its value
+		cFlagNext = cFlag;
+		zFlagNext = zFlag;
 
 		case (op)
 		// MV
 		0: begin
-			out = in1;
+			tmpout = {1'b0, in1};
 		end
 		// ADD
 		1: begin
-			out = in1 + in2;
-			zFlagNext = (out == 16'd0);
+			tmpout = in1 + in2;
+			zFlagNext = (tmpout == 17'd0);
+			cFlagNext = (tmpout[16] == 1'b1);
 		end
 		endcase
 	end
