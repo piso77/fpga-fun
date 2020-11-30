77723faf3a7ea4ff4032a50f0c737243843e5ba3
Paolo Pisati <paolo.pisati@canonical.com>
2020-11-30 13:07:57 +0100

    attopu: nanoasm: simplify syntax so we can start using label for jumps
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- attopu/fib8.asm
+++ attopu/fib8.asm
@@ -4,9 +4,11 @@
 .len	1024
 .width	16
 
-LD	r0, #$0
-LD	r1, #$1
-ADD	r2, r0, r1	; newA <= A + B
-MV	r0, r1		; A <= B
-MV	r1, r2		; B <= newA
-BR	NC, #$2
+Start:
+		LD	r0, $0
+		LD	r1, $1
+Loop:
+		ADD	r2, r0, r1	; newA <= A + B
+		MV	r0, r1		; A <= B
+		MV	r1, r2		; B <= newA
+		BR	NZ, Loop
--- attopu/test.asm
+++ attopu/test.asm
@@ -3,12 +3,12 @@
 .len	1024
 .width	16
 
-LD	r0, #$0a		; ld rd, $data	-- rd = $data
-LD	r1, #$02
+LD	r0, $0a		; ld rd, $data	-- rd = $data
+LD	r1, $02
 ADD	r2, r0, r1
 ST	[r0], r2		; st [ra], rs	-- MEM[ra] = rs
-LD	r3, #$0a
+LD	r3, $0a
 LD	r1, [r3]		; ld rd, [ra]	-- rd = MEM[ra]
 MV	r2, r3
-BR	NZ, #$3
+BR	NZ, $3
 RESET
