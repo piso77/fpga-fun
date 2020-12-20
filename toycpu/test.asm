6b28e3a2cb005a3f56c1288fe033aa2ee175eb66
Paolo Pisati <paolo.pisati@canonical.com>
2020-12-20 10:45:03 +0100

    toycpu: tb: test over the 128 word boundary
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- toycpu/test.asm
+++ toycpu/test.asm
@@ -3,12 +3,12 @@
 .len	128
 .width	16
 
-LD	r0, $0a		; ld rd, $data	-- rd = $data
+LD	r0, $8a		; ld rd, $data	-- rd = $data
 MV	r2, r0
 LD	r1, $02
 ADD	r2, r1
 ST	[r0], r2		; st [ra], rs	-- MEM[ra] = rs
-LD	r3, $0a
+LD	r3, $8a
 LD	r1, [r3]		; ld rd, [ra]	-- rd = MEM[ra]
 MV	r2, r3
 LD	r2, $3
--- toycpu/toycpu_tb.v
+++ toycpu/toycpu_tb.v
@@ -79,13 +79,13 @@ module test_toycpu;
 	end
 `else
 	initial begin // assertions
-		#2 `assert(reg0,			16'h000a)		// LD  r0, $0a
-		#2 `assert(reg2,			16'h000a)		// MV  r2, r0
+		#2 `assert(reg0,			16'h008a)		// LD  r0, $0a
+		#2 `assert(reg2,			16'h008a)		// MV  r2, r0
 		#2 `assert(reg1,			16'h0002)		// LD  r1, $02
-		#2 `assert(reg2,			16'h000c)		// ADD r2, r1
-		#4 `assert(reg3,			16'h000a)		// LD  r3, $0a
-		#2 `assert(reg1,			16'h000c)		// LD  r1, [r3]
-		#2 `assert(reg2,			16'h000a)		// MV  r2, r3
+		#2 `assert(reg2,			16'h008c)		// ADD r2, r1
+		#4 `assert(reg3,			16'h008a)		// LD  r3, $0a
+		#2 `assert(reg1,			16'h008c)		// LD  r1, [r3]
+		#2 `assert(reg2,			16'h008a)		// MV  r2, r3
 	end
 `endif
 
