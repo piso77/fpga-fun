0787576ecdab7a45ef6273266b355af6b263827a
Paolo Pisati <paolo.pisati@canonical.com>
2020-12-21 17:00:55 +0100

    toycpu: tb: merge back complete test.asm
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- toycpu/test.asm
+++ toycpu/test.asm
@@ -1,8 +1,15 @@
 .arch	toycpu
 .org	0
-.len	8
+.len	128
 .width	16
 
 	LD	r0, $80
+	MV	r2, r0
 	LD	r1, $01
-	ADD	r0, r1
+Loop:
+	ADD	r2, r1
+	ST	[r0], r2
+	LD	r3, $80
+	LD	r0, [r3]
+	LD	r3, Loop
+	BR	nz, [r3]
