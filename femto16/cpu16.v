3cd7b69883387ab1188754fc7e425984ac8b0a56
Paolo Pisati <paolo.pisati@canonical.com>
2021-04-06 15:54:14 +0200

    femto16: load fw externally and fix toplevel name
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- femto16/cpu16.v
+++ femto16/cpu16.v
@@ -303,34 +303,8 @@ module test_CPU16_top(
     else
       to_cpu <= rom[address_bus[7:0]];
   
-`ifdef EXT_INLINE_ASM
-  initial begin
-    rom = '{
-      __asm
-.arch femto16
-.org 0x8000
-.len 256
-      mov	sp,@$6fff
-      mov	dx,@Fib
-      jsr	dx
-      reset
-Fib:
-      mov	ax,#1
-      mov	bx,#0
-Loop:
-      mov	cx,ax
-      add	ax,bx
-      mov	bx,cx
-      push	ax
-      pop	ax
-      mov	[42],ax
-      mov	ax,[42]
-      bcc	Loop
-      rts
-      __endasm
-    };
-  end
-`endif
+	initial
+		$readmemh("fib16.hex", rom);
 
 endmodule
 
